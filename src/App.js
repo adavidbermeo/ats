bdb756cb9e402eccf9feb6023959586fda5ec384
adavidbermeo <adavidbermeo@gmail.com>
2021-03-23 23:00:02 -0500

    my first custom component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Tasks from './sample/tasks.json';
+import TaskComponent from './components/TaskComponent';
+//console.log(Tasks);
 
-class People extends React.Component{
+class App extends Component{
 
   state = {
-    show : true
-  }
-
-  toggleShow = () => {
-   this.setState({show: !this.state.show});
+    tasks: Tasks
   }
 
   render(){
-    if(this.state.show){
-      return(
-        <div class="container-people">
-          <h1>Name: {this.props.name}</h1>
-          <h2>Lastname: {this.props.lastname}</h2>
-          <button onClick={this.toggleShow}>Active ToggleShow</button>
-        </div>
-      );
-    }else{
-      return(
-        <div class="no-data-container">
-          <h2>There is no data</h2>
-          <button onClick={this.toggleShow}>Active ToggleShow</button>
-        </div>
-      );
-    }
+    return  <div>
+      {this.state.tasks.map((e)=> <p key={e.id}> 
+        {e.title} - {e.description} - {e.done} - {e.id}
+        <TaskComponent />
+       </p>)}
+    </div>
   }
 }
 
-
-function App() {
-  return (
-    <div class="container-app">
-        <People name="Angel David" lastname="Bermeo" />
-        <People name="Maria Jose" lastname="Muñoz" />
-        <People name="Juan Sebastian" lastname="Santanilla" />
-        <People name="Sandra Jimena" lastname="Garcia" />
-    </div>
-  );
-}
 
 export default App;
---
+++ src/components/TaskComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class TaskComponent extends Component{
+    render(){
+        return(
+            <h1>Retorna un titulo</h1>
+        );
+    }
+}
+
+export default TaskComponent;
