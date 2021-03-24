77c2f753e6bf1829e7c52144edcc6c7b4a402134
adbermeo <adbermeo@seedem.co>
2021-03-23 21:59:45 -0500

    my first example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+class People extends React.Component{
+
+  state = {
+    show : true
+  }
+
+  toggleShow = () => {
+   this.setState({show: !this.state.show});
+  }
+
+  render(){
+    if(this.state.show){
+      return(
+        <div class="container-people">
+          <h1>Name: {this.props.name}</h1>
+          <h2>Lastname: {this.props.lastname}</h2>
+          <button onClick={this.toggleShow}>Active ToggleShow</button>
+        </div>
+      );
+    }else{
+      return(
+        <div class="no-data-container">
+          <h2>There is no data</h2>
+          <button onClick={this.toggleShow}>Active ToggleShow</button>
+        </div>
+      );
+    }
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container-app">
+        <People name="Angel David" lastname="Bermeo" />
+        <People name="Maria Jose" lastname="Muñoz" />
+        <People name="Juan Sebastian" lastname="Santanilla" />
+        <People name="Sandra Jimena" lastname="Garcia" />
     </div>
   );
 }
